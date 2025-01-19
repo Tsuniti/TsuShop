6695e29bf4c750f6b6d9839e383734ca008c00e3
Tsuniti <Tsuniti00@gmail.com>
2025-01-20 00:18:41 +0100

    Add LoginRequestModel and RegisterRequestModel

---
+++ src/BackEnd/Models/LoginRequestModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TsuShopWebApi.Models;
+
+public class LoginRequestModel
+{
+    [Required]
+    public string Username { get; set; }
+    [Required]
+    public string Password { get; set; }
+}
---
+++ src/BackEnd/Models/RegisterRequestModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TsuShopWebApi.Models;
+
+public class RegisterRequestModel
+{
+    [Required]
+    [MinLength(6)]
+    public string Username { get; set; }
+    [Required]
+    [MinLength(6)]
+    [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",         ErrorMessage = "Password must be at least 8 characters long and contain at least one letter, one number, and one special character.")]
+    public string Password { get; set; }
+}
