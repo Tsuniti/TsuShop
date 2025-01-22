6d467cbea1eac65296713d2324edcd56af874f52
Tsuniti <Tsuniti00@gmail.com>
2025-01-23 00:14:47 +0100

    add AddSomeInCartRequestModel.cs

---
+++ src/BackEnd/Models/CartModels/AddSomeInCartRequestModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TsuShopWebApi.Models.CartModels;
+
+public class AddSomeInCartRequestModel
+{
+    [Required]
+    public Guid ProductId { get; set; }
+    
+    [Required]
+    [Range(0, int.MaxValue, ErrorMessage = "Quantity must be a non-negative number.")]
+    public int Quantity { get; set; }
+}
